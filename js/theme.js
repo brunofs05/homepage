9c37cb517b09eafa74f1b26cdb99cac64d5e7791
Bruno Ferreira <brunoferreira17k@gmail.com>
2026-08-14 15:44:24 -0300

    att the theme configs

--- js/theme.js
+++ js/theme.js
@@ -1,19 +1,49 @@
-
-// document.documentElement retorna o elemento raiz, o html
-const html = document.documentElement;
+// DOM := Document Object Model
+// é uma interface que representa página web em formato
+// de objetos, permitindo que linguagens como o js modifiquem
+// a estrutura
 
 // query selector procura um elemento usando um seletor css
 // neste caso por id
 const lightButton = document.querySelector("#theme-light");
 const darkButton = document.querySelector("#theme-dark");
+const systemButton = document.querySelector("#theme-system");
 
+const html = document.documentElement;
+const metaTag = document.querySelector('[name="color-scheme"]');
+const savedScheme = localStorage.getItem("colorScheme");
 
-// adciona um evento ouvinte
-// faz sentido esse nome kkkk
-darkButton.addEventListener("click", () => {
-  html.setAttribute("data-theme", "dark")
+if (savedScheme) {
+  metaTag.setAttribute("content", savedScheme)
+}
+
+function setTheme(theme) {
+  if (theme == "system") {
+    html.removeAttribute("data-theme");
+    metaTag.setAttribute("content", "light dark");
+    localStorage.removeItem("colorScheme");
+    return;
+  }
+
+  console.log("teste")
+
+  html.setAttribute("data-theme", theme);
+  metaTag.setAttribute("content", theme);
+  localStorage.setItem("colorScheme", theme);
+}
+
+if (savedScheme ){
+ setTheme(savedScheme); 
+}
+
+lightButton.addEventListener("click", ()=> {
+  setTheme("light"); 
 })
 
-lightButton.addEventListener("click", () => {
-  html.setAttribute("data-theme", "light")
+darkButton.addEventListener("click", () => {
+  setTheme("dark");
+})
+
+systemButton.addEventListener("click", () => {
+  setTheme("system")
 })
