59c1907a98bc0a07c18c118757f4300799e39757
Bruno Ferreira <brunoferreira17k@gmail.com>
2026-08-17 12:24:07 -0300

    add aboutme section

--- js/theme.js
+++ js/theme.js
@@ -3,14 +3,23 @@
 // de objetos, permitindo que linguagens como o js modifiquem
 // a estrutura
 
-// query selector procura um elemento usando um seletor css
-// neste caso por id
+// querySelector é um método do objetivo documet, ele recebe um
+// - CSS selector - e retorna o primeiro elemento correspondente
+// pode ser  id, classe, elemento, atributo
 const lightButton = document.querySelector("#theme-light");
 const darkButton = document.querySelector("#theme-dark");
 const systemButton = document.querySelector("#theme-system");
 
-const html = document.documentElement;
+// color-scheme é o nome de um atributo html, lembre-se:
+// <meta name="color-scheme" content="light dark">
 const metaTag = document.querySelector('[name="color-scheme"]');
+
+// document é uma instância da classe HTMLDocument
+// documentElement é uma propriedade de acesso (getter) desse objeto
+// retorna o elemento raiz do documento html, que é a tag html
+const html = document.documentElement;
+
+
 const savedScheme = localStorage.getItem("colorScheme");
 
 if (savedScheme) {
@@ -19,16 +28,20 @@ if (savedScheme) {
 
 function setTheme(theme) {
   if (theme == "system") {
+
+    // 'data-' indicia um atributo destinado a armazenar informação
+    // própria da apliaçaão, <html data-theme="dark"> é o estilo que
+    // o css utiliza
     html.removeAttribute("data-theme");
     metaTag.setAttribute("content", "light dark");
     localStorage.removeItem("colorScheme");
     return;
   }
 
-  console.log("teste")
-
   html.setAttribute("data-theme", theme);
   metaTag.setAttribute("content", theme);
+
+  // colorScheme é o primeiro elemento 
   localStorage.setItem("colorScheme", theme);
 }
 
@@ -47,3 +60,15 @@ darkButton.addEventListener("click", () => {
 systemButton.addEventListener("click", () => {
   setTheme("system")
 })
+
+// -- GABARITO -- 
+// HTML
+// ├── color-scheme  → atributo HTML
+// ├── data-theme    → atributo HTML personalizado
+// └── content       → atributo HTML
+
+// JavaScript
+// ├── colorScheme   → nome de uma chave que você escolheu
+// ├── querySelector → método
+// ├── setAttribute  → método
+// └── localStorage  → API/objeto do navegador
